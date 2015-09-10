c8735353f61962e9460b2de78a9e3772d83446e0
BRIAN GERHARDS <BRIANGERHARDS@GMAIL.COM>
2015-09-09 23:21:52 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var rename = require("gulp-rename");
+
+gulp.task('default', ['scripts:src',"tests:src"], function() {});
+
+gulp.task("scripts:src", function () {
+  return gulp.src("src/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("es6Converted"));
+});
+
+gulp.task("tests:src", function () {
+  return gulp.src("spec/src/*.js")
+    .pipe(babel())
+    .pipe(rename(function (path) {
+    	path.basename += "-spec"
+  	}))
+    .pipe(gulp.dest("spec"));
+});
---
+++ src/Item.js
@@ -0,0 +1,11 @@
+export
+default class {
+
+	constructor(id, name, brand, cost, taxExempt) {
+		this.id = id;
+		this.name = name;
+		this.brand = brand;
+		this.cost = cost;
+		this.taxExempt = taxExempt;
+	}
+}

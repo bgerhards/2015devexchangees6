c4f7bbed94408f0dbe7328aefa58745ec91dae4e
= <=>
2015-09-16 09:35:10 -0500

    Updated for the Developer Exchange.

--- spec/src/test.js
+++ spec/src/test.js
@@ -5,30 +5,9 @@ describe("Testing Output", function () {
 
 	it("Canary", function(){})
 
-	it("Creating new classes... Just a quick test! ", function () {
-		// var items = [];
-		items.push(new Item(1, 'Potato', 'Idaho', '.60', true));
-		items.push(new Item(2, 'Tomato', 'Iowa', '.75', true));
-
-		expect(items[0].id).toBe(1);
-		expect(items[0].name).toBe('Potato');
-		expect(items[0].brand).toBe('Idaho');
-		expect(items[0].cost).toBe('.60');
-		expect(items[0].taxExempt).toBe(true);
-
-		// expect(items[1].itemName).toBe('Tomato');
-		// expect(items[1].itemNumber).toBe(2);
-	})
-
-	it("Creating new classes... In an array! ", function () {
-		// var items = [];
-		items.push(new Item(1,'Potato'));
-		items.push(new Item(2,'Tomato'));
-		expect(items[0].name).toBe('Potato');
-		expect(items[0].id).toBe(1);
-		expect(items[1].name).toBe('Tomato');
-		expect(items[1].id).toBe(2);
-	})
+	// it("This is my first test!", function () {
+	// 	expect(this).toBe(something);
+	// })
 
 
 })
--- src/Item.js
+++ src/Item.js
@@ -1,11 +1,6 @@
 export
 default class {
 
-	constructor(id, name, brand, cost, taxExempt) {
-		this.id = id;
-		this.name = name;
-		this.brand = brand;
-		this.cost = cost;
-		this.taxExempt = taxExempt;
+	constructor() {
 	}
 }
